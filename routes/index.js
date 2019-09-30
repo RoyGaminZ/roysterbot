9236bb56b4c2cd96d1d7bc30264f3cc629b2dc36
RoyGaminZ <55430300+RoyGaminZ@users.noreply.github.com>
2019-09-30 13:31:52 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { pageTitle: 'Bongo Cat', user: req.session.user || null });
+    res.render('index', { pageTitle: 'RoySter Bot', user: req.session.user || null });
 });
 
 router.get('/commands', (req, res) => {
-    res.render('commands', { pageTitle: 'Bongo Cat', user: req.session.user || null });
+    res.render('commands', { pageTitle: 'RoySter Bot', user: req.session.user || null });
 });
 
 router.get('*', function(req, res){
-    res.status(404).render('404', { pageTitle: 'Bongo Cat', user: req.session.user || null });
+    res.status(404).render('404', { pageTitle: 'RoySter Bot', user: req.session.user || null });
     });
 
 module.exports = router;
